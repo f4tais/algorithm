250bba29db30574e40c600d70675274877d07a8a
f4tais <55445913+f4tais@users.noreply.github.com>
2020-05-04 23:57:58 +0900

    Create fibonacci.cpp

---
+++ BOJ/fibonacci.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int dp[41];
+int fib(int n);
+
+int main() {
+	int T, N;
+
+	cin >> T;
+	while (T--) {
+		cin >> N;
+		if (N == 0)
+			cout << '1' << ' ' << '0' << endl;
+		else {
+			fib(N);
+			cout << dp[N - 1] << ' ' << dp[N] << endl;
+		}
+	}
+
+	return 0;
+}
+
+int fib(int n) {
+	if (n == 0) {
+		dp[0] = 0;
+		return 0;
+	}
+	else if (n == 1) {
+		dp[1] = 1;
+		return 1;
+	}
+	else if (dp[n] != 0) {
+		return dp[n];
+	}
+	else {
+		return dp[n] = fib(n - 1) + fib(n - 2);
+	}
+}
