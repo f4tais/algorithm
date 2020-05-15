f3a1b816c4b997d48c55a774ee2504058bcdf54b
f4tais <55445913+f4tais@users.noreply.github.com>
2020-05-15 23:46:06 +0900

    Create primeNum.cpp

---
+++ BOJ/primeNum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// BOJ 1929
+int main() {
+	int N, M;
+	int j;
+
+	cin >> N >> M;
+	bool* arr = new bool[M + 1];
+
+	for (int i = 0; i < M + 1; i++) {
+		arr[i] = true;
+	}
+
+	for (int i = 2; i < M + 1; i++) {
+		if (arr[i]) {
+			if ((unsigned int)pow(i, 2) > 1000001) {
+				break;
+			}
+			else {
+				for (j = (int)pow(i, 2); j < M + 1;) {
+					arr[j] = false;
+					j = j + i;
+				}
+			}
+		}
+	}
+
+	if (N == 1) N++;
+
+	for (int i = N; i < M + 1; i++) {
+		if (arr[i] && i >= N)
+			cout << i << "\n";
+	}
+
+	delete[] arr;
+
+	return 0;
+}
