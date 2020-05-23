62b7224600ba75baca5df85878a62a666b27c657
f4tais <55445913+f4tais@users.noreply.github.com>
2020-05-23 22:25:29 +0900

    Create 위장.cpp

---
+++ programmers/위장.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+	int answer = 1;
+	unordered_map <string, int> m;
+
+	for (int i = 0; i < clothes.size(); i++)
+		m[clothes[i][1]]++;
+	for (auto iter = m.begin(); iter != m.end(); iter++)
+		answer *= iter->second + 1;
+	answer--;
+
+	return answer;
+}
